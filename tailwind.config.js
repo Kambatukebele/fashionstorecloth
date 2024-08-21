09aa2f640a83c0acb5cffe09de17212dfa2e9f7e
kamba tukebele <kambatukebele@gmail.com>
2024-08-21 20:34:07 +0200

    update 1 announcement-bar. created html structure for the text to slide, added js slider

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,21 @@ module.exports = {
           xl: '5rem',
           '2xl': '6rem',
         }
-      }
+      },
+      keyframes: {
+        'slide-out': {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        'slide-in': {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+      },
+      animation: {
+        'slide-out': 'slide-out 1s ease forwards',
+        'slide-in': 'slide-in 1s ease forwards',
+      },
     },
   },
   plugins: [
